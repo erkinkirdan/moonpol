c8aa7b95ea01faddec8f0814904b6ab2b6f9ef44
erkinkirdan <erkinkirdan@users.noreply.github.com>
2017-09-28 23:27:01 +0200

    Update dut.lua

--- dut.lua
+++ dut.lua
@@ -16,7 +16,7 @@ local subnets
 local subnetcounter
 local counter
 local loginterval = 10
-local round = 10000
+local round = 10000000
 
 function getsubnetid(ip)
 	local a = bit.rshift(ip, 24)
@@ -175,6 +175,7 @@ function master()
                 	tokenlast = lm.getTime()
                		addtoken(tokeninterval)
 			roundctr = 0
+			print("girdi")
 		end
         end
 	lm.waitForTasks()
