57439777e02929100dc0fb64340a14f7b72799dd
erkinkirdan <erkinkirdan@users.noreply.github.com>
2017-09-29 19:44:20 +0200

    Update gen.lua

--- gen.lua
+++ gen.lua
@@ -9,7 +9,7 @@ local SRC_IP	= "10.0.0.0"
 local DST_IP	= "20.0.0.0"
 local SRC_PORT_BASE	= 1234
 local DST_POR	= 1234
-local PKT_LEN	= 64 --min:42, max:1514
+local PKT_LEN	= 1514 --min:42, max:1514
 local NUM_FLOWS	= 1000
 
 function master()
